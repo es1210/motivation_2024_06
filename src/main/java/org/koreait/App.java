fb6883e5057a1b33a18e8cce2e2467f99287c68f
es1210 <dmstn27034@gmail.com>
2024-07-01 09:57:49 +0900

    ãRq클래스 도입, 명령어 분석 기능 요청

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,40 +1,42 @@
 package org.koreait;
 import org.koreait.motivation.controller.MotivationController;
 import org.koreait.system.controller.SystemController;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
 public class App {
-
-
-
     public void run() {
         System.out.println("== motivation execution ==");
-
         SystemController systemController = new SystemController();
-
         MotivationController motivationController = new MotivationController();
-
         while (true) {
             System.out.print("command) ");
-
             String cmd = Container.getScanner().nextLine().trim();
-
             if (cmd.equals("exit")) {
                 systemController.exit();
                 break;
-            }
-            if (cmd.length() == 0) {
+            } else if (cmd.length() == 0) {
                 System.out.println("명령어 입력해");
                 continue;
             }
             if (cmd.equals("add")) {
                 motivationController.add();
-            }
-            else if (cmd.equals("delete")){
-                motivationController.delete();
-            }
-            else if (cmd.equals("list")) {
+            } else if (cmd.equals("list")) {
                 motivationController.list();
+            } else if (cmd.startsWith("delete")) {
+
+
+                Rq rq = new Rq(cmd);
+
+                System.out.println(rq.getActionMethod());
+                System.out.println(rq.getParams("id"));
+                System.out.println(rq.getParams("source"));
+                System.out.println(rq.getParams("motivation"));
+
+//                motivationController.delete();
+            } else {
+                System.out.println("사용할 수 없는 명령어입니다");
             }
         }
     }
